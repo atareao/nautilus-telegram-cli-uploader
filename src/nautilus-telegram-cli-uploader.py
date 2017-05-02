aca598cec4a25cbe9e284d23aab0a69f726d451f
atareao <lorenzo.carbonell.cerezo@gmail.com>
2017-05-02 09:06:31 +0200

    First telegram-cli

--- src/nautilus-telegram-cli-uploader.py
+++ src/nautilus-telegram-cli-uploader.py
@@ -30,12 +30,16 @@ except Exception as e:
     print(e)
     exit(-1)
 import os
-import ConfigParser
-from PIL import Image
-import telebot
-from telebot.apihelper import ApiException
+import json
 
 from urllib import unquote_plus
+import time
+import pexpect
+import unicodedata
+import sys
+import subprocess
+import shlex
+import re
 
 from threading import Thread
 from gi.repository import GObject
@@ -46,10 +50,8 @@ from gi.repository import Nautilus as FileManager
 
 APP = '$APP$'
 VERSION = '$VERSION$'
-TOKEN = '270805444:AAEwqvaJAiQ8ZFKvgSrGM2-h2lTYwiUkl8Y'
 
-CONFIG_DIR = os.path.join(os.path.expanduser('~'), '.config')
-CONFIG_APP_DIR = os.path.join(CONFIG_DIR, APP)
+CONFIG_APP_DIR = os.path.join(os.path.expanduser('~'), '.telegram-cli')
 CONFIG_FILE = os.path.join(CONFIG_APP_DIR, '{0}.conf'.format(APP))
 if not os.path.exists(CONFIG_APP_DIR):
     os.makedirs(CONFIG_APP_DIR)
@@ -64,6 +66,99 @@ AUDIO_EXTENSIONS = ['.mp3']
 _ = str
 
 
+def select_value_in_combo(combo, value):
+    model = combo.get_model()
+    for i, item in enumerate(model):
+        if value == item[0]:
+            combo.set_active(i)
+            return
+    combo.set_active(0)
+
+
+def get_selected_value_in_combo(combo):
+    model = combo.get_model()
+    return model.get_value(combo.get_active_iter(), 0)
+
+
+def sleep(sleep_time=250):
+    t = int(round(time.time() * 1000))
+    while int(round(time.time() * 1000)) - t < sleep_time:
+        Gtk.main_iteration()
+
+
+def send(media, peer, afile):
+    peer = peer.replace(' ', '_')
+    cmd = 'telegram-cli -C -W -e \'send_{0} {1} "{2}"\''.format(media, peer,
+                                                                afile)
+    args = shlex.split(cmd)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data1, data2 = p.communicate()
+    ansi_escape = re.compile(r'\x1b[^m]*m')
+    data1 = ansi_escape.sub('', data1.decode('utf-8'))
+    data1 = unicodedata.normalize('NFKD', data1).encode('ascii', 'ignore')
+    data2 = ansi_escape.sub('', data2.decode('utf-8'))
+    data2 = unicodedata.normalize('NFKD', data2).encode('ascii', 'ignore')
+    if data1.find('All done') > -1 and len(data2) == 0:
+        return True
+    return False
+
+
+def send_photo(peer, afile):
+    return send('photo', peer, afile)
+
+
+def send_video(peer, afile):
+    return send('video', peer, afile)
+
+
+def send_audio(peer, afile):
+    return send('audio', peer, afile)
+
+
+def send_file(peer, afile):
+    return send('file', peer, afile)
+
+
+def get_contacts_from_telegram_cli():
+    cmd = 'telegram-cli -C -W -e "contact_list"'
+    args = shlex.split(cmd)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data1, data2 = p.communicate()
+    ansi_escape = re.compile(r'\x1b[^m]*m')
+    ans = ansi_escape.sub('', data1.decode('utf-8'))
+    ans = unicodedata.normalize('NFKD', ans).encode('ascii', 'ignore')
+    print('----')
+    print(ans)
+    print('----')
+    ans = ans.encode('utf-8').replace('\r', '\n').split('\n')[8:-3]
+    contacts = []
+    for element in ans:
+        if element.startswith('> ') or element.startswith(' [') or\
+                element.startswith('  '):
+            pass
+        else:
+            contacts.append(element)
+    contacts = sorted(contacts, key=lambda s: s.lower())
+    return contacts
+
+
+def get_contacts():
+    config = {}
+    contacts = []
+    if os.path.exists(os.path.join(CONFIG_APP_DIR, 'auth')):
+        if os.path.exists(CONFIG_FILE):
+            with open(CONFIG_FILE, 'r') as f:
+                config = json.load(f)
+    if config:
+        contacts = config['contacts']
+    else:
+        contacts = get_contacts_from_telegram_cli()
+        config['contacts'] = contacts
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(config, f)
+    return contacts
+
+
 class IdleObject(GObject.GObject):
     """
     Override GObject.GObject to always emit signals in the main thread
@@ -76,9 +171,9 @@ class IdleObject(GObject.GObject):
         GLib.idle_add(GObject.GObject.emit, self, *args)
 
 
-class UserIDDialog(Gtk.Dialog):
+class PhoneDialog(Gtk.Dialog):
     def __init__(self, parent):
-        Gtk.Dialog.__init__(self, _('User ID'), parent,
+        Gtk.Dialog.__init__(self, _('Telegram-cli'), parent,
                             Gtk.DialogFlags.MODAL |
                             Gtk.DialogFlags.DESTROY_WITH_PARENT,
                             (Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT,
@@ -91,13 +186,112 @@ class UserIDDialog(Gtk.Dialog):
         hbox1 = Gtk.HBox()
         vbox.pack_start(hbox1, True, True, 10)
         #
-        label = Gtk.Label(_('User ID') + ' :')
+        label = Gtk.Label(_('Phone') + ' :')
         hbox1.pack_start(label, True, True, 10)
 
         self.entry = Gtk.Entry()
         hbox1.pack_start(self.entry, True, True, 0)
         #
         self.show_all()
+
+
+class CodeDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, _('Telegram-cli'), parent,
+                            Gtk.DialogFlags.MODAL |
+                            Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                            (Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT,
+                             Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
+        self.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
+        self.set_title(APP)
+        #
+        vbox = Gtk.VBox(spacing=5)
+        self.get_content_area().add(vbox)
+        hbox1 = Gtk.HBox()
+        vbox.pack_start(hbox1, True, True, 10)
+        #
+        label = Gtk.Label(_('Code') + ' :')
+        hbox1.pack_start(label, True, True, 10)
+
+        self.entry = Gtk.Entry()
+        hbox1.pack_start(self.entry, True, True, 0)
+        #
+        self.show_all()
+
+
+class SendDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, _('Telegram-cli'), parent,
+                            Gtk.DialogFlags.MODAL |
+                            Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                            (Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT,
+                             Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
+        self.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
+        self.set_title(APP)
+        #
+        vbox = Gtk.VBox(spacing=5)
+        self.get_content_area().add(vbox)
+        hbox1 = Gtk.HBox()
+        vbox.pack_start(hbox1, True, True, 10)
+        #
+        label = Gtk.Label(_('Select contact') + ' :')
+        hbox1.pack_start(label, True, True, 10)
+
+        contactsstore = Gtk.ListStore(str)
+        for contact in get_contacts():
+            contactsstore.append([contact])
+        self.comboboxcontacts = Gtk.ComboBox.new()
+        self.comboboxcontacts.set_model(contactsstore)
+        cell1 = Gtk.CellRendererText()
+        self.comboboxcontacts.pack_start(cell1, True)
+        self.comboboxcontacts.add_attribute(cell1, 'text', 0)
+        self.comboboxcontacts.set_active(0)
+        hbox1.pack_start(self.comboboxcontacts, True, True, 10)
+        #
+        self.show_all()
+
+    def get_selected(self):
+        return get_selected_value_in_combo(self.comboboxcontacts)
+
+
+class RegisterTelegramCli(IdleObject, Thread):
+    __gsignals__ = {
+        'code': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ()),
+        'ended': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ()),
+    }
+
+    def __init__(self, phone):
+        IdleObject.__init__(self)
+        Thread.__init__(self)
+        self.phone = phone
+        self.code = None
+        self.gotit = False
+        self.stopit = False
+        self.ok = False
+        self.daemon = True
+
+    def set_code(self, code):
+        self.code = code
+        self.gotit = True
+
+    def run(self):
+        try:
+            child = pexpect.spawn('telegram-cli -e contact_list')
+            child.logfile = sys.stdout
+            child.expect_exact('phone number:')
+            child.sendline(self.phone)
+            child.expect_exact("code ('CALL' for phone code):")
+            # child.expect('code ')
+            self.emit('code')
+            print('code')
+            while self.gotit is False:
+                sleep(200)
+            child.sendline(self.code)
+            child.expect('updated flags')
+            child.close()
+        except Exception as e:
+            print(e)
+            self.ok = False
 
 
 class DoItInBackground(IdleObject, Thread):
@@ -108,11 +302,10 @@ class DoItInBackground(IdleObject, Thread):
         'end_one': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (float,)),
     }
 
-    def __init__(self, user_id, files):
+    def __init__(self, peer, files):
         IdleObject.__init__(self)
         Thread.__init__(self)
-        self.tb = telebot.TeleBot(TOKEN)  # create a new Telegram Bot object
-        self.user_id = user_id
+        self.peer = peer
         self.elements = files
         self.stopit = False
         self.ok = False
@@ -121,29 +314,23 @@ class DoItInBackground(IdleObject, Thread):
     def stop(self, *args):
         self.stopit = True
 
+    def run(self):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+            self.ok = False
+
     def send_file(self, file_in):
         filename, file_extension = os.path.splitext(file_in)
         if file_extension.lower() in IMAGE_EXTENSIONS:
-            image_in = Image.open(file_in)
-            basename, old_extension = os.path.splitext(file_in)
-            if old_extension.lower() != '.jpg':
-                temp_image = basename + '.jpg'
-                image_in.convert('RGB').save(temp_image)
-                afile = open(temp_image, 'rb')
-                self.tb.send_photo(self.user_id, afile)
-                os.remove(temp_image)
-            else:
-                afile = open(file_in, 'rb')
-                self.tb.send_photo(self.user_id, afile)
+            self.tb.send_photo(self.peer, file_in)
         elif file_extension.lower() in VIDEO_EXTENSIONS:
-            afile = open(file_in, 'rb')
-            self.tb.send_video(self.user_id, afile)
+            self.tb.send_video(self.peer, file_in)
         elif file_extension.lower() in AUDIO_EXTENSIONS:
-            afile = open(file_in, 'rb')
-            self.tb.send_audio(self.user_id, afile)
+            self.tb.send_audio(self.peer, file_in)
         else:
-            afile = open(file_in, 'rb')
-            self.tb.send_document(self.user_id, )
+            self.tb.send_document(self.peer, file_in)
 
     def run(self):
         total = 0
@@ -254,96 +441,60 @@ def get_files(files_in):
     return files
 
 
-class TelegramUploaderMenuProvider(GObject.GObject, FileManager.MenuProvider):
+class TelegramCliUploaderMenuProvider(GObject.GObject,
+                                      FileManager.MenuProvider):
 
     def __init__(self):
-        config = ConfigParser.ConfigParser()
-        config.read(CONFIG_FILE)
-        if len(config.sections()) == 0:
-            self.user_id = None
-        else:
-            try:
-                self.user_id = config.getint('User', 'ID')
-            except ValueError as e:
-                print(e)
-                self.user_id = None
+        self.contacts = get_contacts()
 
     def all_files_are_files(self, items):
         for item in items:
-            if os.path.isfile(unquote_plus(item.get_uri()[7:])):
+            if not os.path.isfile(unquote_plus(item.get_uri()[7:])):
                 return False
         return True
 
     def send_files(self, menu, selected, window):
         files = get_files(selected)
         if len(files) > 0:
-            diib = DoItInBackground(self.user_id,
-                                    files)
-            progreso = Progreso(_('Send files to telegram'),
-                                window,
-                                len(files))
-            diib.connect('started', progreso.set_max_value)
-            diib.connect('start_one', progreso.set_element)
-            diib.connect('end_one', progreso.increase)
-            diib.connect('ended', progreso.close)
-            progreso.connect('i-want-stop', diib.stop)
-            diib.start()
-            progreso.run()
-
-    def login_to_telegram(self, menu, window):
-        uid = UserIDDialog(window)
-        if uid.run() == Gtk.ResponseType.ACCEPT:
-            self.user_id = int(uid.entry.get_text())
-            uid.destroy()
-            if os.path.exists(CONFIG_FILE):
-                os.remove(CONFIG_FILE)
-            config = ConfigParser.ConfigParser()
-            config.add_section('User')
-            config.set('User', 'ID', user_id)
-            with open(CONFIG_FILE, 'w') as configfile:
-                config.write(configfile)
-
-    def unlogin_from_telegram(self, menu):
-        if os.path.exists(CONFIG_FILE):
-            os.remove(CONFIG_FILE)
-        self.user_id = None
+            sd = SendDialog(window)
+            if sd.run() == Gtk.ResponseType.ACCEPT:
+                contact = sd.get_selected.replace(' ', '_')
+                diib = DoItInBackground(contact,
+                                        files)
+                progreso = Progreso(_('Send files to telegram'),
+                                    window,
+                                    len(files))
+                diib.connect('started', progreso.set_max_value)
+                diib.connect('start_one', progreso.set_element)
+                diib.connect('end_one', progreso.increase)
+                diib.connect('ended', progreso.close)
+                progreso.connect('i-want-stop', diib.stop)
+                diib.start()
+                progreso.run()
 
     def get_file_items(self, window, sel_items):
         top_menuitem = FileManager.MenuItem(
-            name='TelegramUploaderMenuProvider::Gtk-telegram-top',
+            name='TelegramCliUploaderMenuProvider::Gtk-telegram-top',
             label=_('telegram...'),
             tip=_('Send files to telegram'))
         submenu = FileManager.Menu()
         top_menuitem.set_submenu(submenu)
-
         sub_menuitem_00 = FileManager.MenuItem(
-            name='TelegramUploaderMenuProvider::Gtk-telegram-sub-00',
+            name='TelegramCliUploaderMenuProvider::Gtk-telegram-sub-00',
             label=_('Send...'),
             tip='Send files to telegram')
         sub_menuitem_00.connect('activate', self.send_files, sel_items,
                                 window)
         submenu.append_item(sub_menuitem_00)
 
-        if self.all_files_are_files(sel_items) and self.user_id is None:
-            sub_menuitem_00.set_property('sensitive', False)
-        else:
+        if self.all_files_are_files(sel_items) and \
+                os.path.exists(os.path.join(CONFIG_APP_DIR, 'auth')):
             sub_menuitem_00.set_property('sensitive', True)
-        if self.user_id is None:
-            sub_menuitem_01 = FileManager.MenuItem(
-                name='TelegramUploaderMenuProvider::Gtk-telegram-sub-01',
-                label=_('Login'),
-                tip='Login to telegram to send images')
-            sub_menuitem_01.connect('activate', self.login_to_telegram, window)
         else:
-            sub_menuitem_01 = FileManager.MenuItem(
-                name='TelegramUploaderMenuProvider::Gtk-telegram-sub-01',
-                label=_('Unlogin'),
-                tip='Unlogin from telegram')
-            sub_menuitem_01.connect('activate', self.unlogin_from_telegram)
-        submenu.append_item(sub_menuitem_01)
+            sub_menuitem_00.set_property('sensitive', False)
 
         sub_menuitem_02 = FileManager.MenuItem(
-            name='TelegramUploaderMenuProvider::Gtk-telegram-sub-02',
+            name='TelegramCliUploaderMenuProvider::Gtk-telegram-sub-02',
             label=_('About'),
             tip=_('About'))
         sub_menuitem_02.connect('activate', self.about, window)
@@ -383,77 +534,9 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 if __name__ == '__main__':
-    import telebot
-    from telebot.apihelper import ApiException
-    config = ConfigParser.ConfigParser()
-    config.read(CONFIG_FILE)
-    if len(config.sections()) == 0:
-        config.add_section('User')
-        config.set('User', 'ID', '')
-        with open(CONFIG_FILE, 'w') as configfile:
-            config.write(configfile)
-            user_id = -1
-    else:
-        try:
-            user_id = config.getint('User', 'ID')
-            print(user_id)
-        except ValueError as e:
-            print(e)
-            user_id = -1
-    if user_id == -1:
-        uid = UserIDDialog(None)
-        if uid.run() == Gtk.ResponseType.ACCEPT:
-            user_id = int(uid.entry.get_text())
-            uid.destroy()
-            config.set('User', 'ID', user_id)
-            with open(CONFIG_FILE, 'w') as configfile:
-                config.write(configfile)
-    TOKEN = '270805444:AAEwqvaJAiQ8ZFKvgSrGM2-h2lTYwiUkl8Y'
-    try:
-        tb = telebot.TeleBot(TOKEN) #create a new Telegram Bot object
-        photo = open('/home/lorenzo/Escritorio/telegram-cli.jpg', 'rb')
-        tb.send_photo(user_id, photo)
-    except ApiException as e:
-        print(e)
-        
-    '''
-    uid = UserIDDialog(None)
-    if uid.run() == Gtk.ResponseType.ACCEPT:
-        code = int(uid.entry.get_text())
-        uid.destroy()
-        TOKEN = '270805444:AAEwqvaJAiQ8ZFKvgSrGM2-h2lTYwiUkl8Y'
-        try:
-            tb = telebot.TeleBot(TOKEN) #create a new Telegram Bot object
-            tb.send_message(code, 'sample 2')
-            photo = open('/home/lorenzo/Escritorio/telegram-cli.jpg')
-            tb.send_photo(code, photo)
-        except ApiException as e:
-            print(e)
-    '''
-    '''
-    def temporal(ipt):
-        print('aqui')
-        cd = CodeDialog(None)
-        if cd.run() == Gtk.ResponseType.ACCEPT:
-            code =cd.entry.get_text()
-            cd.destroy()
-            print(code)
-            ipt.set(code)
-    pd = PhoneDialog(None)
-    if pd.run() == Gtk.ResponseType.ACCEPT:
-        phone = pd.entry.get_text()
-        pd.destroy()
-        time.sleep(1)
-        print(phone)
-        ipt = InputPhoneThread(phone)
-        ipt.connect('code', temporal)
-        ipt.run()
-
-        cd = CodeDialog(None)
-        if cd.run() == Gtk.ResponseType.ACCEPT:
-            code =cd.entry.get_text()
-            cd.destroy()
-            print(code)
-            ipt.set(code)
-        '''
-
+    # print(send_file('atareao', '/home/lorenzo/PDF/pendientes.pdf'))
+    sd = SendDialog(None)
+    if sd.run() == Gtk.ResponseType.ACCEPT:
+        print(sd.get_selected())
+        print(send_file('{0}'.format(sd.get_selected()),
+              '/home/lorenzo/PDF/pendientes.pdf'))
